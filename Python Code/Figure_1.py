d1077296644b0462bfa5c1cf11a9c67b3b264984
Thomas Edwards <tedwards2412@gmail.com>
2022-02-10 17:34:36 +0000

    Updating some plotting

--- Python Code/Figure_1.py
+++ Python Code/Figure_1.py
@@ -5,31 +5,38 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-rc('text', usetex=True)
-rc('font',**{'family':'sans-serif','sans-serif':['cmr']})
-rc('font',**{'family':'serif','serif':['cmr']})
-rc('font', size=18)
+plt.style.use("plot_style.mplstyle")
 
-massrange = np.logspace(-1,2)
-kroupa = np.where(massrange<0.5, massrange**-1.3, 0.5*massrange**-2.3)
+massrange = np.logspace(-1, 2)
+kroupa = np.where(massrange < 0.5, massrange ** -1.3, 0.5 * massrange ** -2.3)
 kroupa /= integ.simps(kroupa, x=massrange)
-chabrier = np.where(massrange<1, 0.158*(1/(np.log(10)*massrange))*np.exp(-(np.log10(massrange)-np.log10(0.08))**2/(2*0.69**2)), 0.158*(1/(np.log(10)*1.0))*np.exp(-(np.log10(1.0)-np.log10(0.08))**2/(2*0.69**2))*massrange**-2.3)
-chabrier /= integ.simps(chabrier, x= massrange)
-varying18 = np.where(massrange<1, massrange**-1.3, massrange**-1.8)
+chabrier = np.where(
+    massrange < 1,
+    0.158
+    * (1 / (np.log(10) * massrange))
+    * np.exp(-((np.log10(massrange) - np.log10(0.08)) ** 2) / (2 * 0.69 ** 2)),
+    0.158
+    * (1 / (np.log(10) * 1.0))
+    * np.exp(-((np.log10(1.0) - np.log10(0.08)) ** 2) / (2 * 0.69 ** 2))
+    * massrange ** -2.3,
+)
+chabrier /= integ.simps(chabrier, x=massrange)
+varying18 = np.where(massrange < 1, massrange ** -1.3, massrange ** -1.8)
 varying18 /= integ.simps(varying18, x=massrange)
-varying235 = np.where(massrange<1, massrange**-1.3, massrange**-2.35)
+varying235 = np.where(massrange < 1, massrange ** -1.3, massrange ** -2.35)
 varying235 /= integ.simps(varying235, x=massrange)
 
-plt.figure(figsize=(7,5))
-fig, ax = plt.subplots()
-plt.plot(massrange, kroupa, label='Kroupa (2001)', ls = ':', color = 'C2')
-plt.plot(massrange, chabrier, label='Chabrier (2003)', ls = '-.', color = 'C3')
-plt.fill_between(massrange, varying18, varying235, alpha=0.3, label='Varying IMF', color = 'C0')
-plt.plot(massrange, varying235, label='Salpeter (this work)', ls = '-', color='C0')
+plt.figure()
+# fig, ax = plt.subplots()
+plt.loglog(massrange, kroupa, label="Kroupa (2001)", ls=":", color="C2")
+plt.loglog(massrange, chabrier, label="Chabrier (2003)", ls="-.", color="C3")
+plt.fill_between(
+    massrange, varying18, varying235, alpha=0.3, label="Varying IMF", color="C0"
+)
+plt.loglog(massrange, varying235, label="Salpeter (this work)", ls="-", color="C0")
 
-plt.yscale('log')
-plt.xscale('log')
-plt.legend(fontsize=12, loc='lower left')
-plt.ylabel(r'IMF ($dN/dM$)')
-plt.xlabel(r'Mass [$M_\odot$]')
-plt.savefig('../plots/IMF.pdf', bbox_inches='tight')
+plt.xlim(1e-1, 1e2)
+plt.legend(loc="lower left")
+plt.ylabel(r"IMF ($dN/dM$)")
+plt.xlabel(r"Mass [$M_\odot$]")
+plt.savefig("../plots/IMF.pdf", bbox_inches="tight")
--- Python Code/Figure_4.py
+++ Python Code/Figure_4.py
@@ -5,51 +5,103 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-rc('text', usetex=True)
-rc('font',**{'family':'sans-serif','sans-serif':['cmr']})
-rc('font',**{'family':'serif','serif':['cmr']})
-rc('font', size=18)
+plt.style.use("plot_style.mplstyle")
 
-z = np.linspace(0,4, 100)
+z = np.linspace(0, 4, 100)
 
-rhoSF_spiral = sn.RSF_density(z,gtype='spiral')
-rhoSF_starburst = sn.RSF_density(z,gtype='starburst')
-rhoSF_AGNspiral = sn.RSF_density(z,gtype='AGN spiral')
-rhoSF_AGNstarburst = sn.RSF_density(z,gtype='AGN starburst')
-rhoSF = (rhoSF_spiral+rhoSF_starburst+rhoSF_AGNspiral+rhoSF_AGNstarburst)
+rhoSF_spiral = sn.RSF_density(z, gtype="spiral")
+rhoSF_starburst = sn.RSF_density(z, gtype="starburst")
+rhoSF_AGNspiral = sn.RSF_density(z, gtype="AGN spiral")
+rhoSF_AGNstarburst = sn.RSF_density(z, gtype="AGN starburst")
+rhoSF = rhoSF_spiral + rhoSF_starburst + rhoSF_AGNspiral + rhoSF_AGNstarburst
 
-rhoSF_spiral_sal = sn.RSF_density(z,gtype='spiral',usesalpeter=True)
-rhoSF_starburst_sal = sn.RSF_density(z,gtype='starburst',usesalpeter=True)
-rhoSF_AGNspiral_sal = sn.RSF_density(z,gtype='AGN spiral',usesalpeter=True)
-rhoSF_AGNstarburst_sal = sn.RSF_density(z,gtype='AGN starburst',usesalpeter=True)
-rhoSF_sal = (rhoSF_spiral_sal+rhoSF_starburst_sal+rhoSF_AGNspiral_sal+rhoSF_AGNstarburst_sal)
+rhoSF_spiral_sal = sn.RSF_density(z, gtype="spiral", usesalpeter=True)
+rhoSF_starburst_sal = sn.RSF_density(z, gtype="starburst", usesalpeter=True)
+rhoSF_AGNspiral_sal = sn.RSF_density(z, gtype="AGN spiral", usesalpeter=True)
+rhoSF_AGNstarburst_sal = sn.RSF_density(z, gtype="AGN starburst", usesalpeter=True)
+rhoSF_sal = (
+    rhoSF_spiral_sal
+    + rhoSF_starburst_sal
+    + rhoSF_AGNspiral_sal
+    + rhoSF_AGNstarburst_sal
+)
 
-m11x, m11xer, m11y, m11yerlo, m11yerhi = np.loadtxt('../Data/SN_constraints/Magnelli_2011.txt', unpack = True)
-m13x, m13xer, m13y, m13yerlo, m13yerhi = np.loadtxt('../Data/SN_constraints/Magnelli_2013.txt', unpack = True)
-gx, gxer, gy, gyerlo, gyerhi = np.loadtxt('../Data/SN_constraints/Gruppioni_2013.txt', unpack = True)
+m11x, m11xer, m11y, m11yerlo, m11yerhi = np.loadtxt(
+    "../Data/SN_constraints/Magnelli_2011.txt", unpack=True
+)
+m13x, m13xer, m13y, m13yerlo, m13yerhi = np.loadtxt(
+    "../Data/SN_constraints/Magnelli_2013.txt", unpack=True
+)
+gx, gxer, gy, gyerlo, gyerhi = np.loadtxt(
+    "../Data/SN_constraints/Gruppioni_2013.txt", unpack=True
+)
 
 
-plt.figure(figsize=(7,5))
+plt.figure(figsize=(7, 5))
 fig, ax = plt.subplots()
 
-plt.errorbar(m11x, 10**m11y, xerr=m11xer, yerr=[10**m11y-10**(m11y+m11yerlo), 10**(m11y+m11yerhi)-10**m11y], label = 'Magnelli et al. (2011)', color = 'red', marker = 'o', ls='none')
-plt.errorbar(m13x, 10**m13y, xerr=m13xer, yerr=[10**m13y-10**(m13y+m13yerlo), 10**(m13y+m13yerhi)-10**m13y], label = 'Magnelli et al. (2013)', color = 'red', marker = 's', ls='none')
-plt.errorbar(gx, 10**gy, xerr=gxer, yerr=[10**gy-10**(gy+gyerlo), 10**(gy+gyerhi)-10**gy], label = 'Gruppioni et al. (2013)', color = 'darkred', marker = 'p', ls='none')
+plt.errorbar(
+    m11x,
+    10 ** m11y,
+    xerr=m11xer,
+    yerr=[10 ** m11y - 10 ** (m11y + m11yerlo), 10 ** (m11y + m11yerhi) - 10 ** m11y],
+    label="Magnelli et al. (2011)",
+    color="red",
+    marker="o",
+    ls="none",
+)
+plt.errorbar(
+    m13x,
+    10 ** m13y,
+    xerr=m13xer,
+    yerr=[10 ** m13y - 10 ** (m13y + m13yerlo), 10 ** (m13y + m13yerhi) - 10 ** m13y],
+    label="Magnelli et al. (2013)",
+    color="red",
+    marker="s",
+    ls="none",
+)
+plt.errorbar(
+    gx,
+    10 ** gy,
+    xerr=gxer,
+    yerr=[10 ** gy - 10 ** (gy + gyerlo), 10 ** (gy + gyerhi) - 10 ** gy],
+    label="Gruppioni et al. (2013)",
+    color="darkred",
+    marker="p",
+    ls="none",
+)
+print(z)
+print((rhoSF_sal - rhoSF) / rhoSF_sal)
 
-plt.plot(z,rhoSF,'-', color='C0',label='Varying IMF')
-plt.plot(z,rhoSF_sal,'--', color='C2', label='Salpeter IMF')
+plt.plot(z, rhoSF, "-", color="C0", label="Varying IMF")
+plt.plot(z, rhoSF_sal, "--", color="C2", label="Salpeter IMF")
 
-plt.scatter(m11x, 10**m11y * [rhoSF[z<m11x[i]][-1]/rhoSF_sal[z<m11x[i]][-1] for i in range(len(m11x))], color='C0')
-plt.scatter(m13x, 10**m13y * [rhoSF[z<m13x[i]][-1]/rhoSF_sal[z<m13x[i]][-1] for i in range(len(m13x))], color='C0')
-plt.scatter(gx, 10**gy * [rhoSF[z<gx[i]][-1]/rhoSF_sal[z<gx[i]][-1] for i in range(len(gx))], color='C0')
+plt.scatter(
+    m11x,
+    10 ** m11y
+    * [rhoSF[z < m11x[i]][-1] / rhoSF_sal[z < m11x[i]][-1] for i in range(len(m11x))],
+    color="C0",
+)
+plt.scatter(
+    m13x,
+    10 ** m13y
+    * [rhoSF[z < m13x[i]][-1] / rhoSF_sal[z < m13x[i]][-1] for i in range(len(m13x))],
+    color="C0",
+)
+plt.scatter(
+    gx,
+    10 ** gy
+    * [rhoSF[z < gx[i]][-1] / rhoSF_sal[z < gx[i]][-1] for i in range(len(gx))],
+    color="C0",
+)
 
-#plt.arrow(1.75, 0.085, 0, -0.04, length_includes_head=True, color = 'firebrick', width = 0.1, head_width = 0.2, head_length = 0.02, alpha = 0.4)
-#plt.text(1.87, 0.048, r'\noindent Observed $R_\mathrm{SF}$ expected\\ to change assuming\\ non-Salpeter IMF', horizontalalignment='left')
+# plt.arrow(1.75, 0.085, 0, -0.04, length_includes_head=True, color = 'firebrick', width = 0.1, head_width = 0.2, head_length = 0.02, alpha = 0.4)
+# plt.text(1.87, 0.048, r'\noindent Observed $R_\mathrm{SF}$ expected\\ to change assuming\\ non-Salpeter IMF', horizontalalignment='left')
 
-plt.yscale('log')
-plt.xlim(0,3.0)
-plt.ylim(1e-2,4e-1)
-plt.legend(loc='lower right', fontsize=10, ncol=1)
-plt.xlabel(r'$z$')
-plt.ylabel(r'$R_{\mathrm{SF}} \, \mathrm{[M_\odot\,yr^{-1}\,Mpc^{-3}]}$')
-plt.savefig('../plots/R_SF_v2.pdf', bbox_inches='tight')
+plt.yscale("log")
+plt.xlim(0, 3.0)
+plt.ylim(1e-2, 4e-1)
+plt.legend(loc="lower right", fontsize=10, ncol=1)
+plt.xlabel(r"$z$")
+plt.ylabel(r"$R_{\mathrm{SF}} \, \mathrm{[M_\odot\,yr^{-1}\,Mpc^{-3}]}$")
+plt.savefig("../plots/R_SF_v2.pdf", bbox_inches="tight")
